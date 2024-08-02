f6c3f178ad0c3d0155f76494e6c9137d204636f0
Unvire <krzysztof.balcerzak6@gmail.com>
2024-08-02 11:43:51 +0200

    init commit

---
+++ dataContainer.py
@@ -0,0 +1,26 @@
+class DataContainer():
+    def __init__(self, name:str):
+        self.name = name
+        self.data = {}
+        self.lowerLimit = None
+        self.upperLimit = None
+    
+    def setLimits(self, lowerLimit:float, upperLimit:float):
+        self.lowerLimit = float(lowerLimit)
+        self.upperLimit = float(upperLimit)
+
+    def addData(self, site:str, value:float):
+        self.data.setdefault(site, [])
+        self.data[site].append(float(value))
+    
+    def getDataFromSite(self, site:str) -> list[float]:
+        return self.data[site]
+    
+    def getDataFromAllSites(self) -> list[str]:
+        result = []
+        for _, values in self.data.items():
+            result += values
+        return result
+
+if __name__ == '__main__':
+    a = DataContainer('test')
