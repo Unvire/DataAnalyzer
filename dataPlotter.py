4cd2a5d93fab4ced5e66114bb7522363d1349302
Unvire <krzysztof.balcerzak6@gmail.com>
2024-08-02 15:17:36 +0200

    Adds drawing sequence plot

--- dataPlotter.py
+++ dataPlotter.py
@@ -1,13 +1,27 @@
 import matplotlib.pyplot as plt
 
 class DataPlotter:
-    def sequencePlot(self, dataList:list[float]):
+    def sequencePlot(self, dataList:list[float], title:str, limits:list[float, float]):
+        lowerLimitValue, upperLimitValue = limits
+        numberOfSamples = len(dataList)
+        lowerLimitList = [lowerLimitValue] * numberOfSamples
+        upperLimitList = [upperLimitValue] * numberOfSamples
+
+        plt.plot(dataList, '.', linewidth=1)
+        plt.plot(lowerLimitList)
+        plt.plot(upperLimitList)
+        plt.title(title)
+        plt.xlim([0, numberOfSamples])
+        plt.xlabel('Sample')
+        plt.ylabel('Value')
+        plt.legend(['Data', 'Lower limit', 'Upper limit'])
+        plt.grid()
+        plt.show()
+
+    def normalDistributionPlot(self, dataList:list[float], title:str, limits:list[float, float]):
         pass
 
-    def normalDistributionPlot(self, dataList:list[float]):
-        pass
-
-    def normalProbabilityPlot(self, dataList:list[float]):
+    def normalProbabilityPlot(self, dataList:list[float], title:str, limits:list[float, float]):
         pass
 
 if __name__ == '__main__':
@@ -21,4 +35,4 @@ if __name__ == '__main__':
     data = speaFileProcessor.getTestMeasurements('L7-1 -> INDL7 20m 20% -20%')
 
     plotter = DataPlotter()
-    plotter.sequencePlot(data.getDataFromSite('1'))
+    plotter.sequencePlot(data.getDataFromSite('1'), data.name, data.getLimits())
