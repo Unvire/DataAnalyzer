a9153838161a11facd83fa8e995303a45fb91715
Unvire <krzysztof.balcerzak6@gmail.com>
2024-08-02 15:49:27 +0200

    Adds drawing capability analisis plot

--- dataPlotter.py
+++ dataPlotter.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import norm
+import seaborn as sns
 
 class DataPlotter:
     def sequencePlot(self, dataList:list[float], title:str, limits:list[float, float]):
@@ -19,7 +22,25 @@ class DataPlotter:
         plt.show()
 
     def normalDistributionPlot(self, dataList:list[float], title:str, limits:list[float, float]):
-        pass
+        lowerLimitValue, upperLimitValue = limits
+        mean = np.mean(dataList)
+
+        x = np.linspace(np.min(dataList), np.max(dataList), 2 * len(dataList))
+        y = norm.pdf(x, loc=5, scale=1)
+
+        plt.figure(figsize=(15,10))
+        plt.figure().set_figheight(9)
+        plt.hist(dataList, bins=10, density=True, edgecolor='black', alpha=0.7, label='Measurements')
+        sns.kdeplot(dataList, color="blue", label="Density ST")
+        plt.plot(x, y, linestyle="--", color="black", label="Theorethical Density ST")
+        plt.axvline(lowerLimitValue, linestyle="--", color="red", label="LSL")
+        plt.axvline(upperLimitValue, linestyle="--", color="orange", label="USL")
+        plt.axvline(mean, linestyle="--", color="green", label="Mean")
+        plt.title(title)
+        plt.xlabel('Value')
+        plt.ylabel('Probability Density')
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
+        plt.show()
 
     def normalProbabilityPlot(self, dataList:list[float], title:str, limits:list[float, float]):
         pass
@@ -35,4 +56,5 @@ if __name__ == '__main__':
     data = speaFileProcessor.getTestMeasurements('L7-1 -> INDL7 20m 20% -20%')
 
     plotter = DataPlotter()
-    plotter.sequencePlot(data.getDataFromSite('1'), data.name, data.getLimits())
+    #plotter.sequencePlot(data.getDataFromSite('1'), data.name, data.getLimits())
+    plotter.normalDistributionPlot(data.getDataFromSite('1'), data.name, data.getLimits())
