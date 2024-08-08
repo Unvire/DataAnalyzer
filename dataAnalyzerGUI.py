f0be6c34e3bcbf5509740a60cf78a6e9a07fabc8
Unvire <krzysztof.balcerzak6@gmail.com>
2024-08-08 11:25:02 +0200

    Adds data to comboBox and bindsgetting folder path to a button

--- dataAnalyzerGUI.py
+++ dataAnalyzerGUI.py
@@ -1,9 +1,12 @@
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
+
 from PyQt5 import QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
 from templateDataAnalyzerGUI import Ui_MainWindow
-import numpy as np
+from fileProcessorFactory import FileProcessorsFactory
 
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -17,14 +20,26 @@ class DataAnalyzerGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         super(DataAnalyzerGUI, self).__init__()
         self.setupUi(self)
 
+        FILE_PROCESSORS = ['SPEA']
+        for fileName in FILE_PROCESSORS:
+            self.comboBox.addItem(fileName)
+        
+        self.pushButton.clicked.connect(self.selectFolder)
+
         self.canvas = MplCanvas(self.plotFrame)
         self.plot_layout = QtWidgets.QVBoxLayout(self.plotFrame)
         self.plot_layout.addWidget(self.canvas)
-
         self.plot()
+    
+    def selectFolder(self):
+        folderPath = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory"))
+        if folderPath:
+            self.processLogsInFolder(folderPath)
+    
+    def processLogsInFolder(self, folderPath:str):
+        print(folderPath)
 
     def plot(self):
-        # Tworzenie przykładowego wykresu
         t = np.arange(0.0, 2.0, 0.01)
         s = np.sin(2 * np.pi * t)
         self.canvas.ax.plot(t, s)
