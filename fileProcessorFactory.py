2096707293164c74353fc545c7a871b4acf53467
Unvire <krzysztof.balcerzak6@gmail.com>
2024-08-08 11:30:07 +0200

    Implements observer pattern

--- fileProcessorFactory.py
+++ fileProcessorFactory.py
@@ -3,13 +3,22 @@ import dataContainer
 import speaDataProcessor
 
 class FileProcessorsFactory:
-    def __init__(self, loaderType:str):
+    def __init__(self):
         self.dataProcessorsDict = {
             'SPEA': speaDataProcessor.SpeaDataProcessor
         }
-
+        self.observersList = []
+    
+    def setProcessorType(self, loaderType:str):
         self.loaderInstance = self.dataProcessorsDict[loaderType]()
     
+    def addObserver(self, instance:object):
+        self.observersList.append(instance)
+
+    def updateObservers(self, progress:int):
+        for observer in self.observersList:
+            observer.setProgressBar(progress)
+
     def processAllLogsInFolder(self, folderPath:str):
         for file in os.listdir(folderPath):
             logPath = os.path.join(folderPath, file)
