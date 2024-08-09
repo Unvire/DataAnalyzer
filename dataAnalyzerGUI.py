b1da17ac25e58d0022c18047e932aa1e35334de8
Unvire <krzysztof.balcerzak6@gmail.com>
2024-08-09 09:39:10 +0200

    implements sequence plot based on Data Plotter class

--- dataAnalyzerGUI.py
+++ dataAnalyzerGUI.py
@@ -7,7 +7,6 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 from templateDataAnalyzerGUI import Ui_MainWindow
 from fileProcessorFactory import FileProcessorsFactory
-from dataPlotter import DataPlotter
 
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -22,15 +21,18 @@ class DataAnalyzerGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.measurements = {}
+        self.selectedTest = ''
+
         self.factory = FileProcessorsFactory()
         self.factory.addObserver(self)
 
         FILE_PROCESSORS = ['Select file type', 'SPEA']
         for fileName in FILE_PROCESSORS:
             self.comboBox.addItem(fileName)
+
         self.comboBox.currentTextChanged.connect(self.selectProcessor)
-        
         self.pushButton.clicked.connect(self.selectFolder)
+        self.listWidget.itemClicked.connect(lambda item: self.listWidgetClickedEvent(item))
 
         self.canvas = MplCanvas(self.plotFrame)
         self.plot_layout = QtWidgets.QVBoxLayout(self.plotFrame)
@@ -55,6 +57,48 @@ class DataAnalyzerGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setMeasurements(measurements)
         self.generateMeasurementsList()
         self.updateNumOfSites()
+
+    def listWidgetClickedEvent(self, item):
+        self.selectedTest = item.text()
+        self.generatePlot()
+
+    def generatePlot(self):
+        generatePlot = {'Sequence plot':self._sequencePlot, 
+                        'Capability plot': self._capabilityPlot}
+
+        testName = self.selectedTest
+        site = '1'
+        plotType = 'Sequence plot'
+
+        data = self.measurements[testName]        
+        dataList = data.getDataFromSite(site)
+        limits = data.getLimits()
+        
+        generatePlot[plotType](dataList, testName, limits)
+
+    def _sequencePlot(self, dataList:list[float], title:str, limits:list[float, float]):
+        numberOfSamples = len(dataList)
+        self.canvas.ax.cla()
+        self.canvas.ax.plot(dataList, '.', linewidth=1, label=f'Data ({numberOfSamples} samples)')
+        self.canvas.ax.set_xlim([0, numberOfSamples])
+        self.canvas.ax.grid()
+        self._addCommonPlotElements(title, limits, False, ['Sample', 'Value'])
+    
+    def _capabilityPlot(self, dataList:list[float], title:str, limits:list[float, float]):
+        pass
+    
+    def _addCommonPlotElements(self, title:str, limits:tuple[float], isLimitsVertical:bool, axisLabels:tuple[str]):
+        limitHandles = {True: self.canvas.ax.axvline, False:self.canvas.ax.axhline}
+        
+        lowerLimitValue, upperLimitValue = limits
+        xLabel, yLabel = axisLabels
+        limitHandles[isLimitsVertical](lowerLimitValue, linestyle='--', color='red', label='LSL')
+        limitHandles[isLimitsVertical](upperLimitValue, linestyle='--', color='orange', label='USL')
+        self.canvas.ax.set_title(title)
+        self.canvas.ax.set_xlabel(xLabel)
+        self.canvas.ax.set_ylabel(yLabel)
+        self.canvas.ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
+        self.canvas.draw()
     
     def updateProgressBar(self, progressPercent:int):
         self.progressBar.setProperty("value", progressPercent)
