c5c81c431db49d69add1d33cc8b7aac7a616dda8
Unvire <krzysztof.balcerzak6@gmail.com>
2024-09-04 12:00:22 +0200

    adds common method for creating new data container

--- abstractDataProcessor.py
+++ abstractDataProcessor.py
@@ -11,6 +11,12 @@ class AbstractDataProcessor(metaclass=abc.ABCMeta):
     def getMeasurements(self) -> dict[str:dataContainer.DataContainer]:
         return self.measurements
 
+    def createDataContainer(self, testName:str, lowerLimit:str|float, upperLimit:str|float):
+        if testName not in self.measurements:
+            testContainer = dataContainer.DataContainer(testName)
+            testContainer.setLimits(lowerLimit, upperLimit)
+            self.measurements[testName] = testContainer
+
     @abc.abstractmethod
     def processLogFile(self, filePath:str):
         pass
