0d76c223ccc2709b4605cac45979b6d3da882731
Unvire <krzysztof.balcerzak6@gmail.com>
2024-09-04 12:29:04 +0200

    Catches error caused by a ";" as a last character in line

--- columnDataProcessor.py
+++ columnDataProcessor.py
@@ -7,8 +7,9 @@ class ColumnDataProcessor(AbstractDataProcessor):
 
     def processLogFile(self, filePath:str):
         with open(filePath, 'r', encoding='unicode_escape') as file:
-            fileLines = file.readlines().replace('\n', '')
+            fileLines = file.readlines()
         
+        fileLines = [line.replace('\n', '') for line in fileLines]
         testName, lowerLimit, upperLimit = self._getSiteLimitsFromHeader(fileLines)
         self.createDataContainer(testName, lowerLimit, upperLimit)
             
@@ -18,9 +19,10 @@ class ColumnDataProcessor(AbstractDataProcessor):
             except ValueError:
                 pass
     
-    def _processFileLine(self, measuredValue:str, testName:str):
+    def _processFileLine(self, fileLine:str, testName:str):
+        measuredValue, *_ = fileLine.split(';')
         self.measurements[testName].addData('1', measuredValue)
     
     def _getSiteLimitsFromHeader(self, fileLines:list[str]) -> tuple[str, str, str]:
-        testName, lowerLimit, upperLimit = fileLines.pop(0).split(';')
+        testName, lowerLimit, upperLimit, *_ = fileLines.pop(0).split(';')
         return testName, lowerLimit, upperLimit
