33f84625fe963957d83a9490d6fd0bdac6792720
Unvire <krzysztof.balcerzak6@gmail.com>
2024-09-04 12:10:43 +0200

    updates type hints

--- dataContainer.py
+++ dataContainer.py
@@ -8,14 +8,14 @@ class DataContainer():
     def getName(self) -> str:
         return self.name
     
-    def setLimits(self, lowerLimit:float, upperLimit:float):
+    def setLimits(self, lowerLimit:float|int|str, upperLimit:float|int|str):
         self.lowerLimit = float(lowerLimit)
         self.upperLimit = float(upperLimit)
     
     def getLimits(self) -> list[float, float]:
         return [self.lowerLimit, self.upperLimit]
 
-    def addData(self, site:str, value:float):
+    def addData(self, site:str, value:float|int|str):
         self.data.setdefault(site, [])
         self.data[site].append(float(value))
     
