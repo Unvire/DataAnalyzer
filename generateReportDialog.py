17a2f939243c1c19a8ceeb5a7765416c6956a774
Unvire <krzysztof.balcerzak6@gmail.com>
2025-01-15 13:37:30 +0100

    Adds class for handling generating report dialog window

---
+++ generateReportDialog.py
@@ -0,0 +1,34 @@
+import sys, os
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel
+from PyQt5 import uic
+
+from testListWrapper import TestListWrapper
+from dataContainer import DataContainer
+
+class generateReportDialog(QDialog):
+    def __init__(self, measurementsDict:dict[str:DataContainer]):
+        super().__init__()
+        uiFilePath = os.path.join(os.getcwd(), 'ui', 'generateRaportDialog.ui')
+        uic.loadUi(uiFilePath, self)
+
+        self.measurements = measurementsDict
+        testNames = self._getTestNames()
+
+        self.testListWrapper = TestListWrapper(self.listWidget, self.filterTestsButton, self.resetFilterButton, self.regexPatternEdit)
+        self.testListWrapper.setRowOnClickEvent(self.listWidgetClickedEvent)
+
+        self.testListWrapper.setTestNames(testNames)
+        self.testListWrapper.generateMeasurementsList()
+    
+    def listWidgetClickedEvent(self, *args):
+        pass
+    
+    def _getTestNames(self) -> list[str]:
+        return list(self.measurements.keys())
+
+if __name__ == '__main__':
+    mockDict = {'a':'a', 'b':'b', 'c':'c', 'd':'d'}
+    app = QApplication(sys.argv)
+    window = generateReportDialog(mockDict)
+    window.show()
+    sys.exit(app.exec_())
