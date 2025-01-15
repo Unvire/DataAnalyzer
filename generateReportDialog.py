366a3d2428f9c808a0843fd445ff3e90cfdf8c4f
Unvire <krzysztof.balcerzak6@gmail.com>
2025-01-15 13:54:29 +0100

    Adds logic for checkbox

--- generateReportDialog.py
+++ generateReportDialog.py
@@ -19,12 +19,24 @@ class generateReportDialog(QDialog):
 
         self.testListWrapper.setTestNames(testNames)
         self.testListWrapper.generateMeasurementsList()
+
+        self._setStatusOfTestsHandlingWidgets(False)
+        self.allTestsCheckBox.toggled.connect(lambda state: self._setStatusOfTestsHandlingWidgets(not state))
     
     def listWidgetClickedEvent(self, *args):
         pass
     
     def _getTestNames(self) -> list[str]:
         return list(self.measurements.keys())
+
+    def _allTestsCheckboxValueChanged(self, state):
+        self._setStatusOfTestsHandlingWidgets(state)
+
+    def _setStatusOfTestsHandlingWidgets(self, status:bool):
+        self.listWidget.setEnabled(status)
+        self.filterTestsButton.setEnabled(status)
+        self.resetFilterButton.setEnabled(status)
+        self.regexPatternEdit.setEnabled(status)
 
 if __name__ == '__main__':
     mockDict = {'a':'a', 'b':'b', 'c':'c', 'd':'d'}
