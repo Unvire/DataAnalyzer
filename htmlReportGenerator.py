e78cf94d226dd3726b1b1609b876acaab07d834f
Unvire <krzysztof.balcerzak6@gmail.com>
2025-01-16 10:35:48 +0100

    updates html code and implements generating plots

--- htmlReportGenerator.py
+++ htmlReportGenerator.py
@@ -1,5 +1,10 @@
-from dataContainer import DataContainer
+import io, base64
+import matplotlib.pyplot as plt
+
+from mplCanvas import MplCanvas
 from processCalculator import ProcessParameterCalculator
+from plotGenerator import SequencePlotGenerator, CapabilityPlotGenerator
+from dataContainer import DataContainer
 
 class HtmlReportGenerator:
     def __init__(self):
@@ -38,7 +43,6 @@ class HtmlReportGenerator:
                 td img {
                     width: 100%;
                     height: auto;
-                    max-width: 120px;
                 }
             </style>
         </head>
@@ -48,52 +52,72 @@ class HtmlReportGenerator:
         </body>
         </html>
         '''
-    
+
     def generateHtmlReport(self, measurementsDict:dict[str:DataContainer], site:int) -> str:
         buffer = self.htmlHead
+        i, iEnd = 0, len(measurementsDict)
         for _, data in measurementsDict.items():
-            buffer += self._generateTable(data, site)
+            try:
+                buffer += self._generateTable(data, site)
+            except Exception as e:
+                print(e)
+            print((i + 1) / iEnd * 100)
+            i+=1
         buffer += self.htmlEnd
         return buffer
         
     def _generateTable(self, data:DataContainer, site:int) -> str:
         title = data.name
-        if site == '0':
-            dataList = data.getDataFromAllSites()
-        else:
-            dataList = data.getDataFromSite(site)
+        dataList = data.getDataFromAllSites() if site == '0' else data.getDataFromSite(site)
 
         lowerLimit, upperLimit = data.getLimits()
         mean, sigmaOverall, pp, ppk, cp, cpk = self.processParameterCalculator.calculate(dataList, lowerLimit, upperLimit)
         
-        siteStr = site if site else 'All sites'
-        #<td colspan="2"><img src="obraz1.jpg" alt="Obraz 1"></td>
-        #<td colspan="2"><img src="obraz2.jpg" alt="Obraz 2"></td>
+        sequencePlotBase64 = self._generatePlot('sequence', dataList, lowerLimit, upperLimit)
+        capabilityPlotBase64 = self._generatePlot('capability', dataList, lowerLimit, upperLimit)
+        
+        siteStr = site if site != '0' else 'All sites'
         htmlSubtable = f'''
         <div class="table-container">
             <table>
                 <tr>
-                    <th colspan="2">{title}</th>
-                    <th colspan="2">Site: {siteStr}</th>
+                    <th colspan="3">{title}</th>
+                    <th>Site: {siteStr}</th>
                 </tr>
                 <tr>
-                    <th colspan="2">a</th>
-                    <th colspan="2">b</th>
+                    <td colspan="2"><img src="data:image/png;base64,{sequencePlotBase64}" width="400"></td>
+                    <td colspan="2"><img src="data:image/png;base64,{capabilityPlotBase64}" width="400"></td>
                 </tr>
                 <tr>
-                    <td>LSL: {lowerLimit}</td>
-                    <td>x̄: {mean}</td>
-                    <td>pp: {pp}</td>
-                    <td>cp: {cp}</td>
+                    <td>LSL={lowerLimit}</td>
+                    <td>x̄={mean}</td>
+                    <td>pp={pp}</td>
+                    <td>cp={cp}</td>
                 </tr>
                 <tr>
-                    <td>USL: {upperLimit}</td>
-                    <td>σ: {sigmaOverall}</td>
-                    <td>ppk: {ppk}</td>
-                    <td>cpk: {cpk}</td>
+                    <td>USL={upperLimit}</td>
+                    <td>σ={sigmaOverall}</td>
+                    <td>ppk={ppk}</td>
+                    <td>cpk={cpk}</td>
                 </tr>
             </table>
+            </br>
         </div>
         '''
         return htmlSubtable
+    
+    def _generatePlot(self, plotType:str, dataList:list[float], lowerLimit:float, upperLimit:float) -> bytes:
+        plotTypeDict = {
+            'sequence': SequencePlotGenerator,
+            'capability': CapabilityPlotGenerator
+        }
+
+        canvas = MplCanvas()
+        plotGenerator = plotTypeDict[plotType](canvas)
+        plotGenerator.generatePlot(dataList, '', (lowerLimit, upperLimit), False)
+        
+        buffer = io.BytesIO()
+        canvas.savefig(buffer, format='png')    
+        canvas.close()
+        return base64.b64encode(buffer.getvalue()).decode('utf-8')
 
