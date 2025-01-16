b7760eb0388a3115fb41120aedd085eae7469c29
Unvire <krzysztof.balcerzak6@gmail.com>
2025-01-16 10:36:00 +0100

    adds close method

--- mplCanvas.py
+++ mplCanvas.py
@@ -4,7 +4,13 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = plt.figure(figsize=(width, height), dpi=dpi)
-        self.ax = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
+        self.fig = plt.figure(figsize=(width, height), dpi=dpi)
+        self.ax = self.fig.add_subplot(111)
+        super(MplCanvas, self).__init__(self.fig)
         self.setParent(parent)
+    
+    def savefig(self, *args, **kwargs):
+        self.fig.savefig(*args, **kwargs)
+    
+    def close(self):
+        plt.close(self.fig)
