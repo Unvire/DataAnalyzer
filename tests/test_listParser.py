056e1d4700b50e50435cbb894bf4b44ed3db9114
Unvire <krzysztof.balcerzak6@gmail.com>
2025-03-05 14:16:11 +0100

    Adds tests for uniqueBoundaryStrings and processBoundaryStrings after being moved to listParser

--- tests/test_listParser.py
+++ tests/test_listParser.py
@@ -28,6 +28,14 @@ def mockValueSeries():
     ]
     return mockList
 
+@pytest.fixture
+def mockUniqueCxCBoundaryStrings():
+    val1 = '1_1_2_2_3_3_4_4'
+    val2 = '10_10_20_20_30_30_40_40'
+    return [val1, val2]
+
+
+
 def test_mergedDataSeries(mockDataSeries):
     result = listParser.mergedDataSeries(mockDataSeries)
     expectedList = [
@@ -66,4 +74,22 @@ def test_valueDateSeriesToValueSeries(mockValueDateSeries):
 def test_nestedValuesListToFlatValueList(mockValueSeries):
     result = listParser.nestedValuesListToFlatValueList(mockValueSeries)
     expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12]
+    assert result == expected
+
+def test_uniqueBoundaryStrings(mockUniqueCxCBoundaryStrings):
+    val1, val2 = mockUniqueCxCBoundaryStrings
+    inputData = [[val1, val2, val2], [val2], [val2, val2], [val1, val1, val2]]
+
+    result = listParser.uniqueBoundaryStrings(inputData)
+    expected = [val1, val2]
+    assert sorted(result) == sorted(expected)
+
+def test_processBoundaryStrings(mockUniqueCxCBoundaryStrings):    
+    val1, val2 = mockUniqueCxCBoundaryStrings
+    inputData = [val1, val2]
+    result = listParser.processBoundaryStrings(inputData)
+    expected = [
+            [(1, 1), (2, 2), (3, 3), (4, 4), (1, 1)], 
+            [(10, 10), (20, 20), (30, 30), (40, 40), (10, 10)]
+        ]
     assert result == expected
