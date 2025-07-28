3bec05bb4f9a516d9e8e5f29184636f4ccfb7981
Unvire <krzysztof.balcerzak6@gmail.com>
2025-07-28 09:11:06 +0200

    adds docstrings for list parser functions

--- listParser.py
+++ listParser.py
@@ -1,4 +1,24 @@
 def mergedDataSeries(data:list[list[float]]) -> tuple[list[list[tuple[int, float]]], int]:
+    '''
+    Prepares data for  **overlapping series plot**:
+    Converts list of measurements (per site) to tuple (listForPlot, lonestSeriesLength):
+    data:
+    [
+        [val11, val12, val13, ..., val1n], 
+        [val21, val22, val23, ..., val2n], 
+        [val31, val32, val33, ..., val3n], 
+        ...
+    ] 
+
+    List of values ready to be plotted **(overlapping plots)**:
+    [
+        [(1, val11), (2, val12), (3, val13), ..., (n, val1n)], 
+        [(1, val21), (2, val22), (3, val23), ..., (n, val2n)], 
+        [(1, val31), (2, val32), (3, val33), ..., (n, val3n)],
+        ...    
+    ],
+    lonestSeriesLength = max([len(series) for series in result])
+    '''
     result = []
     for dataSeries in data:
         seriesPointList = [(i, value) for i, value in enumerate(dataSeries)]
@@ -7,6 +27,26 @@ def mergedDataSeries(data:list[list[float]]) -> tuple[list[list[tuple[int, float
     return result, numberOfSamples
 
 def continuousDataSeries(data:list[list[float]]) -> tuple[list[list[tuple[int, float]]], int]:
+    '''
+    Prepares data for  **series plot next to each other**:
+    Converts list of measurements (per site) to tuple (listForPlot, lonestSeriesLength):
+    data:
+    [
+        [val11, val12, val13, ..., val1n], 
+        [val21, val22, val23, ..., val2n], 
+        [val31, val32, val33, ..., val3n], 
+        ...
+    ] 
+
+    List of values ready to be plotted **series plot next to each other**:
+    [
+        [(1     , val11), (2     , val12), (3     , val13), ..., (n     , val1n)], 
+        [(n  + 1, val21), (n  + 2, val22), (n  + 3, val23), ..., (n  + n, val2n)], 
+        [(2n + 1, val31), (2n + 2, val32), (2n + 3, val33), ..., (2n + n, val3n)],
+        ...    
+    ],
+    lonestSeriesLength = first item of last tuple in last list (each tuples have index included)
+    '''
     result = []
     offset = 0
     for dataSeries in data:
@@ -17,16 +57,32 @@ def continuousDataSeries(data:list[list[float]]) -> tuple[list[list[tuple[int, f
     return result, numberOfSamples + 1
 
 def flattenDataSeries(data:list[list[float]]) -> tuple[list[float], int]:
-    result = [value for siteData in data for value in siteData]
+    result = nestedValuesListToFlatValueList(data)
     return result, len(result)
 
-def valueDateSeriesToValueSeries(data:list[list[float, str]]) -> list[list[float]]:
-    return [[value for value, _ in siteData] for siteData in data]
-
 def nestedValuesListToFlatValueList(data:list[list[float]]) -> list[float]:
+    '''
+    Prepares data for gauss plot
+    Converts list of measurements to flat 1 dimension list of values:
+    data:
+    [
+        [val11, val12, val13, ..., val1n], 
+        [val21, val22, val23, ..., val2n], 
+        [val31, val32, val33, ..., val3n], 
+        ...
+    ] 
+
+    List of values ready to be plotted **series plot next to each other**:
+    [val11, val12, val13, ..., val1n, val21, val22, val23, ..., val2n, val31, val32, val33, ..., val3n, ...],
+    '''
     return [value for siteData in data for value in siteData]
 
 def uniqueBoundaryStrings(siteBoundariesList:list[list[str]]|str) -> list[str]:
+    '''
+    Processes given boundary strings and ensures that they are all unique.
+    If string is passed then it is wrapped in a list
+    If list is of strings is passed then it is casted to set to remove duplicates
+    '''
     if isinstance(siteBoundariesList, str):
         return [siteBoundariesList]
     
@@ -37,6 +93,10 @@ def uniqueBoundaryStrings(siteBoundariesList:list[list[str]]|str) -> list[str]:
     return list(uniqueBoundaries)
         
 def processBoundaryStrings(uniqueBoundariesList:list[str]) -> list[list[tuple[float, float]]]:
+    '''
+    Converts boundary string to a list with points that describe a 2D closed shape, for example:
+    x1_y1_x2_y2_x3_y3_x4_y4 is converted to [(x1, y1), (x2, y2), (x3, y3), (x4, y4), (x1, y1),] 
+    '''
     result = []
     for boundaryString in uniqueBoundariesList:
         x1, y1, x2, y2, x3, y3, x4, y4 = boundaryString.split('_')
--- tests/test_listParser.py
+++ tests/test_listParser.py
@@ -6,7 +6,7 @@ def mockDataSeries():
     mockList = [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
-        [8, 9, 10 ,11, 12]        
+        [8, 9, 10 ,11, 15]        
     ]
     return mockList
 
@@ -41,7 +41,7 @@ def test_mergedDataSeries(mockDataSeries):
     expectedList = [
         [(0, 1), (1, 2), (2, 3), (3, 4)],
         [(0, 5), (1, 6), (2, 7), (3, 8)],
-        [(0, 8), (1, 9), (2, 10) ,(3, 11), (4, 12)]  
+        [(0, 8), (1, 9), (2, 10) ,(3, 11), (4, 15)]  
     ]
     expectedLength = 5
     assert result == (expectedList, expectedLength)
@@ -51,26 +51,17 @@ def test_continuousDataSeries(mockDataSeries):
     expectedList = [
         [(0, 1), (1, 2), (2, 3), (3, 4)],
         [(4, 5), (5, 6), (6, 7), (7, 8)],
-        [(8, 8), (9, 9), (10, 10) ,(11, 11), (12, 12)]  
+        [(8, 8), (9, 9), (10, 10) ,(11, 11), (12, 15)]  
     ]
     expectedLength = 13
     assert result == (expectedList, expectedLength)
 
 def test_flattenDataSeries(mockDataSeries):    
     result = listParser.flattenDataSeries(mockDataSeries)
-    expectedList = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10 ,11, 12]
+    expectedList = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10 ,11, 15]
     expectedLength = 13
     assert result == (expectedList, expectedLength)
-
-def test_valueDateSeriesToValueSeries(mockValueDateSeries):
-    result = listParser.valueDateSeriesToValueSeries(mockValueDateSeries)
-    expected = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10 ,11, 12]        
-    ]
-    assert result == expected
-
+    
 def test_nestedValuesListToFlatValueList(mockValueSeries):
     result = listParser.nestedValuesListToFlatValueList(mockValueSeries)
     expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12]
